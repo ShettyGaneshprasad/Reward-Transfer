afc768aeb6e32fadac40e2c089f869fdbca2dea6
Shetty Ganeshprasad <shettyganeshprasad1998@gmail.com>
2021-10-05 10:34:52 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:reward_transfer/screens/home_screen.dart';
-import 'package:reward_transfer/screens/login_screen.dart';
-import 'package:reward_transfer/screens/signup_screen.dart';
-import 'package:reward_transfer/screens/welcome_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
+import 'package:reward_transfer/src/bloc/authentication_bloc/authentication_bloc.dart';
+import 'package:reward_transfer/src/bloc/authentication_bloc/authentication_event.dart';
+import 'package:reward_transfer/src/bloc/authentication_bloc/authentication_state.dart';
+import 'package:reward_transfer/src/bloc/simple_bloc_delegate.dart';
+import 'package:reward_transfer/src/repository/user_repository.dart';
+import 'package:reward_transfer/src/ui/home_screen.dart';
+import 'package:reward_transfer/src/ui/login/login_screen.dart';
+import 'package:reward_transfer/src/ui/splash_screen.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+
+  final UserRepository userRepository = UserRepository();
+  runApp(BlocProvider(
+    create: (context) =>
+        AuthenticationBloc(userRepository: userRepository)..add(AppStarted()),
+    child: App(userRepository: userRepository),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
+  final UserRepository? _userRepository;
+
+  App({Key? key, @required UserRepository? userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: 'welcome_screen',
-      routes: {
-        'welcome_screen': (context) => WelcomeScreen(),
-        'registration_screen': (context) => RegistrationScreen(),
-        'login_screen': (context) => LoginScreen(),
-        'home_screen': (context) => HomeScreen()
-      },
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is Uninitialized) {
+            return SplashScreen();
+          }
+          if (state is Authenticated) {
+            return HomeScreen(
+              name: state.displayName,
+            );
+          }
+          if (state is Unauthenticated) {
+            return LoginScreen(
+              userRepository: _userRepository,
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
